965639f926354e46c4728c2a0490c33502322def
CharityMasingi <precious.coder24@gmail.com>
2026-07-21 11:32:06 +0200

    Add ticket booking functionality with user input

--- Unit 1 – Introduction to Python, Strings & Numbers/ticket_booker.py
+++ Unit 1 – Introduction to Python, Strings & Numbers/ticket_booker.py
@@ -1,1 +1,6 @@
+#A program acting as a digital ticket counter
+name = input("Please enter your name:")
+#Name of band/artist they want to see
+band_or_artist = input("Please enter the name of the band/artist you want to see: ")
 
+print (f"Hey {name}! Your tickets to see {band_or_artist} are booked successfully! Have fun!")
