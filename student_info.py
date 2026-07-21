8fa7bf2d0ad26b676d0f258e0d0bc5ce1e587609
CharityMasingi <precious.coder24@gmail.com>
2026-07-21 11:18:44 +0200

    Correct variable types and enhance output formatting
    Fixed variable declaration for favorite number and improved greeting format.

--- student_info.py
+++ student_info.py
@@ -1,7 +1,18 @@
 #Using input to ask for user input
 first_name = input("Please enter your first name: ")
 surname = input("Please enter your surname: ")
- age = int (input("Please enter your age: "))
-float fav_number = input("Please enter your favourite number: ")
+age = int (input("Please enter your age: "))
+fav_number = float (input("Please enter your favourite number: "))
 
-print (f "Welcome , .upper({first_name}), .title({surnanme})")
+#Displaying name with a greeting and making the name in uppercase and the surname in Title 
+print (f"Welcome ,{first_name.upper()} {surname.title()}, !")
+
+new_age = age * 12
+print(f"Your age in months is: {new_age}")
+
+new_fav = round(fav_number, 2)
+
+print(type(first_name))
+print(type(surname))
+print(type(age))
+print(type(fav_number))
