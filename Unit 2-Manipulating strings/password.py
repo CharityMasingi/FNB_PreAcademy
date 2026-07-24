7f5ab44d772a335f62b732ffa7d5a972d92abb0a
CharityMasingi <precious.coder24@gmail.com>
2026-07-24 05:51:52 +0200

    Add password hinting functionality

--- Unit 2-Manipulating strings/password.py
+++ Unit 2-Manipulating strings/password.py
@@ -1,1 +1,12 @@
+#A password hinting script
+password = input("Please enter your password: ")
 
+#removing spaces
+password_stripped = password.strip()
+
+#To take the first and last letters of the password
+first_letter = password_stripped[0]
+last_letter = password_stripped[-1]
+
+#printing the hint
+print (f"Your password starts with {first_letter.upper()} and ends with {last_letter.upper()}.")
